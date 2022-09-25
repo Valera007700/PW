f88518f5b075d8d809a0f463bff35e1c1a7ad693
valery <valeradoroshkov987@gmail.com>
2022-09-25 03:34:43 +0200

    25.09

--- Hallo.py
+++ Hallo.py
@@ -1,10 +1,12 @@
-color = input ("Please type your favorite color: ");
-print(" Your favorite color is\n", color);
+print("lease enter the following:\n");
+adjective = input ("adjective: ");
+animal = input ("animal: \n");
+exclamation = input ("exclamation: \n");
+verb1 = input ("verb: \n");
+verb2 = input ("verb: \n");
 
 
+print(" Your story is:\n");
 
+print("The other day, I was really in trouble. It all started when I saw a very", adjective, animal, "yell down the hallway.", exclamation, "I yelled. But all I could think to do was to", verb1, "over and over. Miraculously, that caused it to stop, but not before it tried to", verb2, "right in front of my family.");
 
-
-#Please type your favorite color: Blue
-#Your favorite color is
-#Blue
