9726fd54670c51205f7780617fe8c10b2be72106
valery <valeradoroshkov987@gmail.com>
2022-10-27 12:11:51 +0200

    2710

--- Game.py
+++ Game.py
@@ -1,20 +1,51 @@
 
-print ("You are walking through a dark forest and find two items: a  MATCH and a FLASHLIGHT. Which one do you want to pick up?")
-#amount = (int(input ("What is your favorite number? ")))
+print ("two items: a  MATCH and a FLASHLIGHT. Which one do you want to pick up?")   
+q = (str(input('I would like take\n')))
 
-answer = (str(input("")))
+if q == "1":                                                                   
+    print("You choosed ", q, "\nAnd right noooow, next question for your trip :) ");
 
-if answer == "MATCH":
-    print("You pick up the match and strike it, and for an instant, the forest around you is illuminated. You see a large grizzly bear, and then the match burns out. Do you want to RUN, or HIDE behind a tree?");
-elif answer == "You pick up the flashlight and turn it on. You see the pathway lit up in front of you, but you thought you also heard something off to the side. Do you want to FOLLOW the path or LOOK in the trees for the thing that made the noise?":
-    print("+++++++");
-elif answer == "match":
-    print("You pick up the match and strike it, and for an instant, the forest around you is illuminated. You see a large grizzly bear, and then the match burns out. Do you want to RUN, or HIDE behind a tree?");
-elif answer == "You pick up the flashlight and turn it on. You see the pathway lit up in front of you, but you thought you also heard something off to the side. Do you want to FOLLOW the path or LOOK in the trees for the thing that made the noise?":
-    print("+++++++");
+        q_1 =  (str(input("1.1 OR 1.2, What would you like?\n")))
+        if q_1 == "1.1":                                                          
+            print("You choosed ", q_1, "\nAnd right noooow, next question for your trip :)" );
+
+            q_2 = (str(input("1.1.1 OR 1.1.2")))
+            if q_2 == "1.1.1":
+                print("1.1.1");
+            elif q_2 == "1.1.2":
+                print("1.1.2");
+            else:
+                print ("get out here!");
 
 
+
+
+    elif q_1 == "1.2":
+        print("1.2");
+        
+        answer_3_2 = (str(input("")))
+        if answer_3_2 == "1.2.1":
+            print("1.2.1");
+        elif answer_3_2 == "1.2.2":
+            print ("1.2.2");
+    else:
+        print("get out of here");
+elif q == "2":
+    print("2");
+    answer_2 =  (str(input("")))
+    if answer_2 == "2.1":
+        print("2.1")
+        answer_2_2 =  (str(input("")))
+        if answer_2_2 == "2.1.1":
+            print("2.1.1")
+        elif answer_2_2 == "2.1.2":
+            print ("2.1.2")
+    elif answer_2 == "2.2":
+        print("2.2");
+        answer_2_4 =  (str(input("")))
+        if answer_2_4 == "2.2.1":
+            print("2.2.1")
+        elif answer_2_4 == "2.2.2":
+            print ("2.2.2")
 else:
-    print("wrong")
-
-ghbdtn gjgjtj 
+    print("wrong");
